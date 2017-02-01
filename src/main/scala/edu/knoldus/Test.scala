ac7f69e6213eec77ad2d507f9b0b96b5fee33931
Charmy garg <charmygarg07@gmail.com>
2017-02-01 21:06:49 +0530

    Delete Test.scala

--- src/main/scala/edu/knoldus/Test.scala
+++
@@ -1,9 +0,0 @@
-package edu.knoldus
-
-object Test extends App{
-
-  val obj = Client(1)
-  //println(obj.insert("e01","shubham","agra","8755925562","d001","proj1"))
-  println(obj.delete("c01"))
-
-}
